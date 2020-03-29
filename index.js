171885695b9dc586608a4f1e7927894ac8b66516
LANHER <61714910+LANHER@users.noreply.github.com>
2020-03-29 17:57:50 -0600

    init
    Ok

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
+const bodyParser = require('body-parser');
 const express= require('express');
 const app = express();
 const {pokemon}= require('./pokedex.json');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+/*
+    GET - Obtener recursos
+    POST- Almacenar recursos
+    PATCH - Modificar una parte de un recurso
+    PUT - Modificar un recurso
+    DELETE - Borrar un recurso
+*/
+
 app.get("/",(req, res, next)=>{
-    res.status(200);
-    res.send("Bienvenido al Pokedex");
+    return res.status(200).send("Bienvenido al Pokedex");
+});
+
+app.post("/pokemon",(req, res, next)=>{
+    return res.status(200).send(req.body);
 });
 
 app.get('/pokemon',(req, res, next)=>{
-    res.status(200);
-    res.send(pokemon);
+    return res.status(200).send(pokemon);
 });
 
-app.get('/pokemon/:id',(req, res, next)=>{
-    res.status(200);
-    res.send(pokemon[req.params.id -1]);
+app.get('/pokemon/:id([0-9]{1,3})',(req, res, next)=>{
+    // concicion ? valor si verdadero: valor si falso
+
+    const id = req.params.id - 1;
+    (id >= 0 && id <= 150)?
+        res.status(200).send(pokemon[req.params.id -1]):
+        res.status(404).send("Pokémon no encontrado");
+    });
+
+app.get('/pokemon/:name([A-Za-z]+)',(req, res, next)=>{
+    const name = req.params.name;
+
+    const pk = pokemon.filter((p) =>{
+        return (p.name.toUpperCase()== name.toUpperCase()) && p;
+    });
+
+    (pk.length >0) ?
+     res.status(200).send(pk) : 
+     res.status(404).send("Pokémon no encontrado");
 
 });
 
