d49a6bb0eb50e6a00849749c7079f05d1267cef2
DavidDonnet <dlanderos10@alumnos.uaq.mx>
2020-05-23 20:22:18 -0500

    Actividad 14

--- index.js
+++ index.js
@@ -9,19 +9,12 @@ const user = require('./routes/user');
 const auth = require('./middleware/auth');
 const notFoud = require('./middleware/notFound')
 const index = require('./middleware/index');
+const cors = require('./middleware/cors')
 
-
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
-/*
-    GET - Obtener recursos
-    POST- Almacenar recursos
-    PATCH - Modificar una parte de un recurso
-    PUT - Modificar un recurso
-    DELETE - Borrar un recurso
-*/
 
 app.get("/", index);
 app.use("/user", user);
---
+++ middleware/cors.js
@@ -0,0 +1,10 @@
+module.exports=(req,res,next)=>{
+    res.header("Acces-Control-Allow-Origin","*");
+    res.header("Acces-Control-Allow-Headers",
+    "Origin, X-Requested-With, ContentType, Accept, Authorization"
+    );
+    if(req.method ==='OPTIONS'){
+        res.header("Acces-Control-Allow-Methods","PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+}
